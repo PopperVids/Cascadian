702110e654812351a65d84ddecf20a4df01f5e0a
PopperVids <uribealexis917@gmail.com>
2024-11-25 01:31:47 -0800

    Initial commit

---
+++ src/main/java/net/PopperMods/Cascadian/CascadianClient.java
@@ -0,0 +1,10 @@
+package net.PopperMods.Cascadian;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CascadianClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
